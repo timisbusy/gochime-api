55149f6fa86360c0caea17b43ad9d8bb9a4e1789
Tim Atkinson <timisbusy@gmail.com>
2013-07-23 14:38:29 -0400

    adding test, conf to gitignore

---
+++ test.js
@@ -0,0 +1,19 @@
+var gochime = require('./index')
+  , conf = require('./conf');
+
+gochime.test(conf.token, function (err, res) {
+  if (err) { throw err; }
+  console.log(res);
+});
+
+gochime.use(conf.token);
+
+gochime.addUsers(conf.testAudience, 'email', ['test@gochime.com'], function (err, res) {
+  if (err) { throw err; }
+  console.log(res);
+});
+
+gochime.addFormattedUsers(conf.testAudience, [{email:'test@gochime.com'}, {phone: '5555555555'}], function (err, res) {
+  if (err) { throw err; }
+  console.log(res);
+});
