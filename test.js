b65a72188c8ed05899c157a5def1974fa12ee6bf
Tim Atkinson <timisbusy@gmail.com>
2013-08-06 12:26:30 -0400

    better logging in test

--- test.js
+++ test.js
@@ -3,17 +3,17 @@ var gochime = require('./index')
 
 gochime.test(conf.token, function (err, res) {
   if (err) { throw err; }
-  console.log(res);
+  console.log('test result: ', res);
 });
 
 gochime.use(conf.token);
 
 gochime.addUsers(conf.testAudience, ['test@gochime.com'], 'email', function (err, res) {
   if (err) { throw err; }
-  console.log(res);
+  console.log('add users result: ',res);
 });
 
-gochime.addFormattedUsers(conf.testAudience, [{email:'test@gochime.com'}, {phone: '5555555555'}], function (err, res) {
+gochime.addFormattedUsers(conf.testAudience, [{email:"test@gochime.com"}, {phone: "5555555555"}], function (err, res) {
   if (err) { throw err; }
-  console.log(res);
+  console.log('formatted users result: ', res);
 });
