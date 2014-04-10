dd252733fec22075916df1a35ddcdb4a3c60410d
Tim Atkinson <timisbusy@gmail.com>
2014-04-10 13:46:29 -0400

    reworking api interface, including some new segmentio specific endpoints

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-var client = require('./lib/client');
+// var client = require('./lib/client');
 
-function test (key, cb) {
-  client.test(key, cb);
-}
+// function test (key, cb) {
+//   client.test(key, cb);
+// }
 
-function use (key) {
-  if (!key) { return console.log('use must include a key!'); }
-  client.use(key);
-}
+// function use (key) {
+//   if (!key) { return console.log('use must include a key!'); }
+//   client.use(key);
+// }
 
-function addUsers (audience, users, type, cb) {
-  client.addFormattedUsers(audience, formatUsers(users, type), cb);
-}
+// function addUsers (audience, users, type, cb) {
+//   client.addFormattedUsers(audience, formatUsers(users, type), cb);
+// }
 
-function addFormattedUsers (audience, formattedUsers, cb) {
-  client.addFormattedUsers(audience, formattedUsers, cb);
-}
+// function addFormattedUsers (audience, formattedUsers, cb) {
+//   client.addFormattedUsers(audience, formattedUsers, cb);
+// }
 
-function removeUsers (audience, users, type, cb) {
-  client.removeFormattedUsers(audience, formatUsers(users, type), cb);
-}
+// function removeUsers (audience, users, type, cb) {
+//   client.removeFormattedUsers(audience, formatUsers(users, type), cb);
+// }
 
-function removeFormattedUsers (audience, formattedUsers, cb) {
-  client.removeFormattedUsers(audience, formattedUsers, cb);
-}
+// function removeFormattedUsers (audience, formattedUsers, cb) {
+//   client.removeFormattedUsers(audience, formattedUsers, cb);
+// }
 
-function formatUsers (users, type) {
-  var formattedUsers = [];
-  users.forEach(function (user) {
-    var fUser = {};
-    fUser[type] = user;
-    formattedUsers.push(fUser);
-  });
-  return formattedUsers;
-}
+// function formatUsers (users, type) {
+//   var formattedUsers = [];
+//   users.forEach(function (user) {
+//     var fUser = {};
+//     fUser[type] = user;
+//     formattedUsers.push(fUser);
+//   });
+//   return formattedUsers;
+// }
 
-module.exports = {
-    use: use
-  , test: test
-  , addUsers: addUsers
-  , addFormattedUsers: addFormattedUsers
-  , formatUsers: formatUsers
-  , removeUsers: removeUsers
-  , removeFormattedUsers: removeFormattedUsers
-}
+// module.exports = {
+//     use: use
+//   , test: test
+//   , addUsers: addUsers
+//   , addFormattedUsers: addFormattedUsers
+//   , formatUsers: formatUsers
+//   , removeUsers: removeUsers
+//   , removeFormattedUsers: removeFormattedUsers
+// }
+
+module.exports = require("./lib/v2");
